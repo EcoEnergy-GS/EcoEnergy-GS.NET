b4d7c6dacbd13dbad664042eff4cf51aa11230b9
gabrielrodri33 <gabrielrodri333@gmail.com>
2024-11-16 12:42:52 -0300

    Criado Model de Historico de pontos

---
+++ EcoEnergy-GS/Models/HistoricoPontosModel.cs
@@ -0,0 +1,18 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace EcoEnergy_GS.Models
+{
+    [Table("Historico_Pontos")]
+    public class HistoricoPontosModel
+    {
+        [Key]
+        public int Id_historico {  get; set; }
+        public DateTime Data { get ; set; }
+        public int quantidade { get; set; }
+
+        [ForeignKey("Usuarios")]
+        [Column("Id_usuario")]
+        public int Id_usuario { get; set; }
+    }
+}
