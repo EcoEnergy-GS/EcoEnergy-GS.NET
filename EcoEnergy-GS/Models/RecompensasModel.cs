e17f920bbfdf02efa4e04e7dad15abce1800af87
gabrielrodri33 <gabrielrodri333@gmail.com>
2024-11-16 12:42:59 -0300

    Criado Model de Recompensas

---
+++ EcoEnergy-GS/Models/RecompensasModel.cs
@@ -0,0 +1,17 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace EcoEnergy_GS.Models
+{
+    [Table("Recompensas")]
+    public class RecompensasModel
+    {
+        [Key]
+        public int Id_recompensas { get; set; }
+
+        [StringLength(100, MinimumLength = 8, ErrorMessage = "A descrição deve ter no máximo 100 caracteres.")]
+        public string Descricao { get; set; }
+        public int Pontos_necessarios { get; set; }
+        public ICollection<TrocasRecompensasModel> Trocas { get; set; }
+    }
+}
