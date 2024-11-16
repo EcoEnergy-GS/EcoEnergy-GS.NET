9df33dc244c257082d64d231318e9752209ce589
gabrielrodri33 <gabrielrodri333@gmail.com>
2024-11-16 12:43:19 -0300

    Criado Model de Tipo de eletrodoméstico

---
+++ EcoEnergy-GS/Models/TipoEletrodomesticoModel.cs
@@ -0,0 +1,21 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace EcoEnergy_GS.Models
+{
+    [Table("Tipo_Eletrodomestico")]
+    public class TipoEletrodomesticoModel
+    {
+        [Key]
+        public int Id_eletrodomestico { get; set; }
+
+        [Required]
+        [StringLength(50, MinimumLength = 8, ErrorMessage = "O dispositivo deve ter no máximo 50 caracteres.")]
+        public string Nome { get; set; }
+
+        [Required]
+        public int quantidade { get; set; }
+
+        public ICollection<ResidenciaModel> Residencia { get; set; }
+    }
+}
