072f508174516d7c948f02a00b0a3b7fadfa6e9b
gabrielrodri33 <gabrielrodri333@gmail.com>
2024-11-16 12:43:31 -0300

    Criado Model de Troca de recompensas

---
+++ EcoEnergy-GS/Models/TrocasRecompensasModel.cs
@@ -0,0 +1,22 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace EcoEnergy_GS.Models
+{
+    [Table("Trocas_recompensas")]
+    public class TrocasRecompensasModel
+    {
+        [Key]
+        public int Id_trocas { get; set; }
+        public DateTime DataTroca { get; set; }
+        public int Pontos_utilizados { get; set; }
+
+        [ForeignKey("Usuarios")]
+        [Column("Id_usuario")]
+        public int Id_usuario { get; set; }
+
+        [ForeignKey("Recompensas")]
+        [Column("Id_recompensas")]
+        public int Id_recompensas { get; set; }
+    }
+}
