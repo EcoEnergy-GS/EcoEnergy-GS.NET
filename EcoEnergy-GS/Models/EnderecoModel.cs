2c656a0749c7276f889ad5e5611c7a311a70ab14
gabrielrodri33 <gabrielrodri333@gmail.com>
2024-11-16 12:42:41 -0300

    Criado Model de Endereco

---
+++ EcoEnergy-GS/Models/EnderecoModel.cs
@@ -0,0 +1,26 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace EcoEnergy_GS.Models
+{
+    [Table("Endereco")]
+    public class EnderecoModel
+    {
+        [Key]
+        public int Id_endereco { get; set; }
+
+        [Required]
+        public string Cep {  get; set; }
+
+        [Required]
+        [StringLength(50, MinimumLength = 8, ErrorMessage = "Nome da rua deve ter no máximo 50 caracteres.")]
+        public string Rua { get; set; }
+
+        [Required]
+        public int Numero { get; set; }
+
+        [StringLength(50, MinimumLength = 8, ErrorMessage = "O complemento deve ter no máximo 50 caracteres.")]
+        public string Complemento { get; set; }
+        public ICollection<ResidenciaModel> Residencia { get; set; }
+    }
+}
