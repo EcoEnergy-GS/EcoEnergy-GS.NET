20276d678667f9d35c31489ea8241eafbdae9460
gabrielrodri33 <gabrielrodri333@gmail.com>
2024-11-16 21:07:20 -0300

    Arrumado relacionamento com Trocas

--- EcoEnergy-GS/Models/RecompensasModel.cs
+++ EcoEnergy-GS/Models/RecompensasModel.cs
@@ -12,6 +12,8 @@ namespace EcoEnergy_GS.Models
         [StringLength(100, MinimumLength = 8, ErrorMessage = "A descrição deve ter no máximo 100 caracteres.")]
         public string Descricao { get; set; }
         public int Pontos_necessarios { get; set; }
+
+        [InverseProperty("Recompensas")]
         public ICollection<TrocasRecompensasModel> Trocas { get; set; }
     }
 }
