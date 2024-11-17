b16ae4e38408b39802d0edf0b238d273a2f669bf
gabrielrodri33 <gabrielrodri333@gmail.com>
2024-11-16 21:06:02 -0300

    Criado DbContext para a API

---
+++ EcoEnergy-GS/Data/AppDbContext.cs
@@ -0,0 +1,19 @@
+﻿using EcoEnergy_GS.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace EcoEnergy_GS.Data
+{
+    public class AppDbContext : DbContext
+    {
+        public AppDbContext(DbContextOptions<AppDbContext> options) : base(options) { }
+
+        public DbSet<UsuarioModel> Usuarios { get; set; }
+        public DbSet<HistoricoPontosModel> HistoricoPontos { get; set; }
+        public DbSet<ResidenciaModel> Residencia { get; set; }
+        public DbSet<ConsumoEnergiaModel> ConsumoEnergia { get;set; }
+        public DbSet<TipoEletrodomesticoModel> TipoEletrodomestico { get; set; }
+        public DbSet<EnderecoModel> Endereco { get; set; }
+        public DbSet<TrocasRecompensasModel> TrocasRecompensas { get; set; }
+        public DbSet<RecompensasModel> Recompensas { get; set; }
+    }
+}
