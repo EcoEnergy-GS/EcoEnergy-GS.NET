1e43d02c68f24f0485939aa335923842863c7b13
gabrielrodri33 <gabrielrodri333@gmail.com>
2024-11-17 22:06:15 -0300

    Criado e configurado migration para rodar as procedures do entregável de banco

--- EcoEnergy-GS/Migrations/20241118004841_initial.Designer.cs
+++ EcoEnergy-GS/Migrations/20241118004841_initial.Designer.cs
@@ -3,6 +3,7 @@ using System;
 using EcoEnergy_GS.Data;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using Oracle.EntityFrameworkCore.Metadata;
 
@@ -11,9 +12,11 @@ using Oracle.EntityFrameworkCore.Metadata;
 namespace EcoEnergy_GS.Migrations
 {
     [DbContext(typeof(AppDbContext))]
-    partial class AppDbContextModelSnapshot : ModelSnapshot
+    [Migration("20241118004841_initial")]
+    partial class initial
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        /// <inheritdoc />
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
