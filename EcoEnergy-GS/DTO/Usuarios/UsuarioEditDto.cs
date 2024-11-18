4c8f3f481be7b9ef1c878ec197f121a1826983bb
gabrielrodri33 <gabrielrodri333@gmail.com>
2024-11-18 16:59:56 -0300

    Criado o EditDto de usuarios

---
+++ EcoEnergy-GS/DTO/Usuarios/UsuarioEditDto.cs
@@ -0,0 +1,23 @@
+﻿using System.Globalization;
+
+namespace EcoEnergy_GS.DTO.Usuarios
+{
+    public class UsuarioEditDto
+    {
+        private string _nome;
+
+        public int id_usuarios { get; set; }
+
+        public string nome
+        {
+            get => _nome;
+            set => _nome = CultureInfo.CurrentCulture.TextInfo.ToTitleCase(value?.ToLower());
+        }
+
+        public string senha { get; set; }
+
+        public string telefone { get; set; }
+
+        public int pontos { get; set; }
+    }
+}
