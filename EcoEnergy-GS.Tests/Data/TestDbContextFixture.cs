68f2df52f64bb8c34ca276fbe5a658cd341aadfe
gabrielrodri33 <gabrielrodri333@gmail.com>
2024-11-18 13:29:10 -0300

    Configurado TestDbContextFixture

---
+++ EcoEnergy-GS.Tests/Data/TestDbContextFixture.cs
@@ -0,0 +1,24 @@
+﻿using EcoEnergy_GS.Data;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EcoEnergy_GS.Tests.Data
+{
+    public class TestDbContextFixture : IDisposable
+    {
+        public AppDbContext Context { get; private set; }
+
+        public TestDbContextFixture()
+        {
+            Context = DbContextFactory.CreateInMemoryDbContext();
+        }
+
+        public void Dispose()
+        {
+            Context.Dispose();
+        }
+    }
+}
