f49af13fb33d35275503157230ce6b49f8018596
gabrielrodri33 <gabrielrodri333@gmail.com>
2024-11-18 13:28:58 -0300

    Configurado DbContextFactory

---
+++ EcoEnergy-GS.Tests/Data/DbContextFactory.cs
@@ -0,0 +1,23 @@
+﻿using EcoEnergy_GS.Data;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EcoEnergy_GS.Tests.Data
+{
+    public class DbContextFactory
+    {
+        public static AppDbContext CreateInMemoryDbContext()
+        {
+            var options = new DbContextOptionsBuilder<AppDbContext>()
+                .UseInMemoryDatabase(databaseName: "TestDB")
+                .Options;
+
+            var context = new AppDbContext(options);
+            return context;
+        }
+    }
+}
