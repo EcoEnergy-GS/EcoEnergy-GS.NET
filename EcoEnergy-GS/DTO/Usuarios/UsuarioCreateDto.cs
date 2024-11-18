e24c91ea792b7e1da607d737d0ca747d381f40c9
gabrielrodri33 <gabrielrodri333@gmail.com>
2024-11-18 16:59:48 -0300

    Criado o CreateDTO de usuarios

---
+++ EcoEnergy-GS/DTO/Usuarios/UsuarioCreateDto.cs
@@ -0,0 +1,24 @@
+﻿using System.ComponentModel.DataAnnotations.Schema;
+using System.ComponentModel.DataAnnotations;
+using System.Globalization;
+using System.Text.Json.Serialization;
+
+namespace EcoEnergy_GS.DTO.Usuarios
+{
+    public class UsuarioCreateDto
+    {
+        private string _nome;
+
+        public string nome
+        {
+            get => _nome;
+            set => _nome = CultureInfo.CurrentCulture.TextInfo.ToTitleCase(value?.ToLower());
+        }
+
+        public string senha { get; set; }
+
+        public string telefone { get; set; }
+
+        public int pontos { get; set; }
+    }
+}
