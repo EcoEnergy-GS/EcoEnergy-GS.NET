fe08ce6839fa7aa4eb8f842754ec24f0363cb144
gabrielrodri33 <gabrielrodri333@gmail.com>
2024-11-17 22:07:47 -0300

    Corrigido a model de troca de recompensas para rodar as procedures

--- EcoEnergy-GS/Models/TrocasRecompensasModel.cs
+++ EcoEnergy-GS/Models/TrocasRecompensasModel.cs
@@ -3,22 +3,27 @@ using System.ComponentModel.DataAnnotations.Schema;
 
 namespace EcoEnergy_GS.Models
 {
-    [Table("Trocas_recompensas")]
+    [Table("TROCAS_RECOMPENSAS")]
     public class TrocasRecompensasModel
     {
         [Key]
-        public int Id_trocas { get; set; }
-        public DateTime DataTroca { get; set; }
-        public int Pontos_utilizados { get; set; }
+        [Column("ID_TROCAS")]
+        public int id_trocas { get; set; }
+
+        [Column("DATA_TROCA")]
+        public DateTime data_troca { get; set; }
+
+        [Column("PONTOS_UTILIZADOS")]
+        public int pontos_utilizados { get; set; }
 
         [ForeignKey("Usuario")]
-        [Column("Id_usuario")]
-        public int Id_usuario { get; set; }
+        [Column("ID_USUARIOS")]
+        public int id_usuarios { get; set; }
         public UsuarioModel Usuario { get; set; }
 
         [ForeignKey("Recompensas")]
-        [Column("Id_recompensas")]
-        public int Id_recompensas { get; set; }
+        [Column("ID_RECOMPENSAS")]
+        public int id_recompensas { get; set; }
         public RecompensasModel Recompensas { get; set; }
     }
 }
