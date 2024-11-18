98519c64814ba05922cb6570e05bbfe1b23c733b
gabrielrodri33 <gabrielrodri333@gmail.com>
2024-11-17 22:07:20 -0300

    Corrigido a model de recompensas para rodar as procedures

--- EcoEnergy-GS/Models/RecompensasModel.cs
+++ EcoEnergy-GS/Models/RecompensasModel.cs
@@ -3,15 +3,19 @@ using System.ComponentModel.DataAnnotations.Schema;
 
 namespace EcoEnergy_GS.Models
 {
-    [Table("Recompensas")]
+    [Table("RECOMPENSAS")]
     public class RecompensasModel
     {
         [Key]
-        public int Id_recompensas { get; set; }
+        [Column("ID_RECOMPENSAS")]
+        public int id_recompensas { get; set; }
 
         [StringLength(100, MinimumLength = 8, ErrorMessage = "A descrição deve ter no máximo 100 caracteres.")]
-        public string Descricao { get; set; }
-        public int Pontos_necessarios { get; set; }
+        [Column("DESCRICAO")]
+        public string descricao { get; set; }
+
+        [Column("PONTOS_NECESSARIOS")]
+        public int pontos_necessarios { get; set; }
 
         [InverseProperty("Recompensas")]
         public ICollection<TrocasRecompensasModel> Trocas { get; set; }
