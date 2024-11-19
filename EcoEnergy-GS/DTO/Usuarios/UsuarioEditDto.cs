743d1155dfd4e7214df7f59a61e5ffeb872a9918
gabrielrodri33 <gabrielrodri333@gmail.com>
2024-11-19 14:26:54 -0300

    Removido as Annotations do UsuarioEditDTO

--- EcoEnergy-GS/DTO/Usuarios/UsuarioEditDto.cs
+++ EcoEnergy-GS/DTO/Usuarios/UsuarioEditDto.cs
@@ -1,4 +1,5 @@
-﻿using System.Globalization;
+﻿using System.ComponentModel.DataAnnotations;
+using System.Globalization;
 
 namespace EcoEnergy_GS.DTO.Usuarios
 {
@@ -6,18 +7,22 @@ namespace EcoEnergy_GS.DTO.Usuarios
     {
         private string _nome;
 
+        [Key]
         public int id_usuarios { get; set; }
 
+        [Required]
         public string nome
         {
             get => _nome;
             set => _nome = CultureInfo.CurrentCulture.TextInfo.ToTitleCase(value?.ToLower());
         }
 
+        [Required]
         public string senha { get; set; }
 
         public string telefone { get; set; }
 
+        [Required]
         public int pontos { get; set; }
     }
 }
