b56ac7e89e53ec0612219e752c152a0116b65bd1
gabrielrodri33 <gabrielrodri333@gmail.com>
2024-11-19 20:52:32 -0300

    Criado a Interface de recompensas

---
+++ EcoEnergy-GS/Services/Recompensas/IRecompensasInterface.cs
@@ -0,0 +1,15 @@
+﻿using EcoEnergy_GS.DTO.Recompensas;
+using EcoEnergy_GS.DTO.Usuarios;
+using EcoEnergy_GS.Models;
+
+namespace EcoEnergy_GS.Services.Recompensas
+{
+    public interface IRecompensasInterface
+    {
+        Task<ResponseModel<List<RecompensasModel>>> ListarRecompensas();
+        Task<ResponseModel<RecompensasModel>> BucarRecompensasPorId(int id_recompensas);
+        Task<ResponseModel<RecompensasModel>> CreateRecompensas(RecompensasCreateDto recompensasCreateDto);
+        Task<ResponseModel<RecompensasModel>> EditRecompensas(RecompensasEditDto recompensasEditDto);
+        Task<ResponseModel<RecompensasModel>> DeleteRecompensas(int id_recompensas);
+    }
+}
