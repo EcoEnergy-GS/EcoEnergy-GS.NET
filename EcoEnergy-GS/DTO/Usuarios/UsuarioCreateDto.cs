3a180f560798bd56dedc44842e8f1f8ea256c541
gabrielrodri33 <gabrielrodri333@gmail.com>
2024-11-19 14:26:30 -0300

    Adicionado validação de senha no UsuarioCreateDTO

--- EcoEnergy-GS/DTO/Usuarios/UsuarioCreateDto.cs
+++ EcoEnergy-GS/DTO/Usuarios/UsuarioCreateDto.cs
@@ -15,6 +15,9 @@ namespace EcoEnergy_GS.DTO.Usuarios
             set => _nome = CultureInfo.CurrentCulture.TextInfo.ToTitleCase(value?.ToLower());
         }
 
+        [StringLength(20, MinimumLength = 8, ErrorMessage = "A senha deve ter entre 8 e 20 caracteres.")]
+        [RegularExpression(@"^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*[@$!%*?&])[A-Za-z\d@$!%*?&]{8,}$",
+            ErrorMessage = "A senha deve conter pelo menos uma letra maiúscula, uma letra minúscula, um número e um caractere especial.")]
         public string senha { get; set; }
 
         public string telefone { get; set; }
