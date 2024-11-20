cd014a19039aa5da9e4fa2e08c82ee6ec886b295
gabrielrodri33 <gabrielrodri333@gmail.com>
2024-11-20 14:38:43 -0300

    Criado métodos com operações de CRUD

---
+++ EcoEnergy-GS/Services/TrocasRecompensas/ITrocasRecompensasInterface.cs
@@ -0,0 +1,14 @@
+﻿using EcoEnergy_GS.DTO.TrocasRecompensas;
+using EcoEnergy_GS.Models;
+
+namespace EcoEnergy_GS.Services.TrocasRecompensas
+{
+    public interface ITrocasRecompensasInterface
+    {
+        Task<ResponseModel<List<TrocasRecompensasModel>>> ListarTrocasRecompensas();
+        Task<ResponseModel<TrocasRecompensasModel>> BucarTrocasRecompensasPorId(int id_trocas);
+        Task<ResponseModel<TrocasRecompensasModel>> CreateTrocasRecompensas(TrocasRecompensasCreateDto trocasRecompensasCreateDto);
+        Task<ResponseModel<TrocasRecompensasModel>> EditTrocasRecompensas(TrocasRecompensasEditDto trocasRecompensasEditDto);
+        Task<ResponseModel<TrocasRecompensasModel>> DeleteTrocasRecompensas(int id_trocas);
+    }
+}
