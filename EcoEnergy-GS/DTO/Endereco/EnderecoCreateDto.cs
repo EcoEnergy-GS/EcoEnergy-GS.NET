bb097d41a15b700ce2c562bc86b3adba1b39fc88
gabrielrodri33 <gabrielrodri333@gmail.com>
2024-11-19 21:56:42 -0300

    Criado DTO de Create para Endereco

---
+++ EcoEnergy-GS/DTO/Endereco/EnderecoCreateDto.cs
@@ -0,0 +1,25 @@
+﻿using System.ComponentModel.DataAnnotations.Schema;
+using System.ComponentModel.DataAnnotations;
+
+namespace EcoEnergy_GS.DTO.Endereco
+{
+    public class EnderecoCreateDto
+    {
+        [Required]
+        [Column("CEP")]
+        public string cep { get; set; }
+
+        [Required]
+        [StringLength(50, MinimumLength = 8, ErrorMessage = "Nome da rua deve ter no máximo 50 caracteres.")]
+        [Column("RUA")]
+        public string rua { get; set; }
+
+        [Required]
+        [Column("NUMERO")]
+        public int numero { get; set; }
+
+        [StringLength(50, MinimumLength = 8, ErrorMessage = "O complemento deve ter no máximo 50 caracteres.")]
+        [Column("COMPLEMENTO")]
+        public string complemento { get; set; }
+    }
+}
